d324be322d160f9e1e24d99bf7a757d68b3c8845
Chris Guest <chris.guest@condenastint.com>
2019-11-02 19:15:58 +0000

    Switch out the container build to be multistage rather than requiring volume mounts.

--- build_base.sh
+++ build_base.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+# using github fork as the docker context.
 docker build -t codelabs_base:latest -f- git@github.com:chrisguest75/codelab-tools-fork.git <<< $(cat Dockerfile)
--- build_codelabs.sh
+++ build_codelabs.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 docker stop "$(docker ps -aqf "name=codelabs")"
-rm -rf dist/*  
-docker run -it -v $(pwd)/dist:/output -v $(pwd)/labs:/codelabs/site/codelabs codelabs_base:latest 
-#docker run -it -v $(pwd)/dist:/output -v $(pwd)/labs:/codelabs/site/codelabs --entrypoint /bin/bash codelabs_base:latest 
 
 docker build -t codelabs:latest -f Dockerfile.codelabs .
 docker run -d --rm -p 8000:80 --name codelabs codelabs
